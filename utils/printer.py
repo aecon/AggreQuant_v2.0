34c493f0965d10cdbd0c94d25d338670a9da5597
Athena Economides <athena.economides@uzh.ch>
2026-02-04 15:13:13 +0100

    Remove old files from previous version

--- utils/printer.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def msg(message, origin):
-    print("\n (%s): %s " % (origin, message))
-
-def err(message, origin):
-    print("\n ERROR (%s): %s " % (origin, message), file=sys.stderr)
